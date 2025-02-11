e80c32c44edabfd2c8e3976fe1a55880619126da
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2025-02-11 12:44:15 +0530

    refactor: simplify CORS middleware configuration in index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://kuraft.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors({
+    origin: 'https://kuraft.vercel.app'
+  }));
 
 
 app.get("/",(req,res)=>{
