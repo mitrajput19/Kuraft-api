66b505cacbe6042c218d0f607e32a6e7bc902e6e
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2025-02-11 12:49:04 +0530

    feat: add custom CORS headers to enhance security and specify allowed methods

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://kuraft.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 
 app.get("/",(req,res)=>{
 return res.status(200).send({message:"welcome to ecommerce api - node"})
