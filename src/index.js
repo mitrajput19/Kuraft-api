1167086fabc219237e054157bbbe6e6cc016164c
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2025-02-11 12:46:51 +0530

    feat: enhance CORS middleware configuration to specify allowed methods and headers

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ app.use(express.json())
 app.use(cors())
 
 app.use(cors({
-    origin: 'https://kuraft.vercel.app'
-  }));
+    origin: 'https://kuraft.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 app.get("/",(req,res)=>{
