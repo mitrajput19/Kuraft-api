1f2ff7a428ab83ce5a0e93225a1e7d17aa328e19
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2025-02-11 11:50:08 +0530

    fix: enhance error handling during database connection in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const app = require(".");
 const { connectDb } = require("./config/db");
 const PORT = 8080;
-app.listen(PORT, async ()=>{
+
+app.listen(PORT, async () => {
+  try {
     await connectDb();
-    console.log("ecommerce api listening on PORT : ",PORT);
-})
+    console.log("ecommerce api listening on PORT:", PORT);
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+});
