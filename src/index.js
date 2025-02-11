9fea5146539990cc2c95125c7d9447477163b4a7
mitrajput19 <mitsinh.rajput@vsit.edu.in>
2025-02-11 12:31:00 +0530

    feat: add CORS middleware for enhanced security and update Vercel configuration to use server.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://kuraft.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 
 app.get("/",(req,res)=>{
 return res.status(200).send({message:"welcome to ecommerce api - node"})
